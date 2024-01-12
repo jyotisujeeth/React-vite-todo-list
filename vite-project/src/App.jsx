9e5a939bb325b2af138d72e210a029aa91d377b8
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-13 00:31:59 +0530

    todo with useState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import "./style.css"
 
 export default function App () {
+  const [newItem, setNewItem] = useState("please add item")
+  //setNewItem("")
+const [toDos, setToDos] = useState([])
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    setToDos((currentToDos) => {
+      return[
+      ...toDos,
+      {
+        id: crypto.randomUUID(), title: newItem, completed:false },
+      
+    ]
+  })
+}
+  
+  //   setToDos((currentToDos) => {
+  //     return[
+  //     ...toDos,
+  //     {
+  //       id: crypto.randomUUID(), title: newItem, completed:false },
+      
+  //   ]
+  // })
+
+  console.log("==========list=====",toDos);
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item"/>
+        <input value={newItem} onChange={e => setNewItem(e.target.value)}type="text" id="item"/>
         </div>   
         <button className="btn">Add</button>
         </form>
